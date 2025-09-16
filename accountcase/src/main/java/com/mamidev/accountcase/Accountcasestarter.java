b4e1830c617db02583b6a34f1fd04869bf711b84
Muhammet Akduman <akdumanmxc@gmail.com>
2025-09-16 21:29:16 +0300

    Initial commit

---
+++ accountcase/src/main/java/com/mamidev/accountcase/Accountcasestarter.java
@@ -0,0 +1,13 @@
+package com.mamidev.accountcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Accountcasestarter {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Accountcasestarter.class, args);
+	}
+
+}
---
+++ accountcase/src/test/java/com/mamidev/accountcase/AccountcaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mamidev.accountcase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountcaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
