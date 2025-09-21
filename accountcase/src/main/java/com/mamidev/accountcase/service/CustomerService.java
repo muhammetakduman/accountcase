cd2d9fc34887a4a17b9e1dc07fd45616cedc2a6b
Muhammet Akduman <akdumanmxc@gmail.com>
2025-09-21 20:21:15 +0300

    Account Controller already

--- accountcase/src/main/java/com/mamidev/accountcase/service/CustomerService.java
+++ accountcase/src/main/java/com/mamidev/accountcase/service/CustomerService.java
@@ -3,7 +3,9 @@ package com.mamidev.accountcase.service;
 import com.mamidev.accountcase.exception.CustomerNotFoundException;
 import com.mamidev.accountcase.model.Customer;
 import com.mamidev.accountcase.repository.CustomerRepository;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CustomerService {
     private final CustomerRepository customerRepository;
 
