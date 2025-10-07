4459bfdbf7b6d5af4aadc4aa9d929c3acb7e8282
Muhammet Akduman <akdumanmxc@gmail.com>
2025-10-08 01:36:29 +0300

    valid entegration , jakarta

--- accountcase/src/main/java/com/mamidev/accountcase/controller/AccountController.java
+++ accountcase/src/main/java/com/mamidev/accountcase/controller/AccountController.java
@@ -3,6 +3,7 @@ package com.mamidev.accountcase.controller;
 import com.mamidev.accountcase.dto.AccountDto;
 import com.mamidev.accountcase.dto.CreateAccountRequest;
 import com.mamidev.accountcase.service.AccountService;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,7 +20,7 @@ public class AccountController {
     }
 
     @PostMapping
-    public ResponseEntity<AccountDto> createAccount(@RequestBody CreateAccountRequest request){
+    public ResponseEntity<AccountDto> createAccount(@Valid @RequestBody CreateAccountRequest request){
         return ResponseEntity.ok(accountService.createAccount(request));
     }
 }
--- accountcase/src/main/java/com/mamidev/accountcase/dto/CreateAccountRequest.kt
+++ accountcase/src/main/java/com/mamidev/accountcase/dto/CreateAccountRequest.kt
@@ -1,8 +1,13 @@
 package com.mamidev.accountcase.dto
 
+import jakarta.validation.constraints.Min
+import jakarta.validation.constraints.NotBlank
 import java.math.BigDecimal
 
 data class CreateAccountRequest(
+    @field:NotBlank(message = "Bu alan boş olamaz")
     val customerId: String,
+
+    @field:Min(0,  message = "0 dan büyük veya eşit bir değer girmelisiniz.")
     val initialCredit: BigDecimal
 )
